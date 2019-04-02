652a6600234196469d61b494b0e6122cc6f4cd1d
Michael Maurizi <mmaurizi@azavea.com>
2019-04-02 19:39:46 -0400

    support 2.2

--- tests/settings.py
+++ tests/settings.py
@@ -14,6 +14,7 @@ DATABASES = {
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.admin',
+    'django.contrib.messages',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
